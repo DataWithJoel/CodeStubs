7665f510e400e49f0b4aa44072db7eaa56b4f49d
JOEL <supriseitsalf@yahoo.com>
2015-11-15 18:09:43 -0800

    Added Basic Transactions

---
+++ Transactions.sql
@@ -0,0 +1,21 @@
+--This is an Example to show basic Transaction syntax
+
+BEGIN TRANSACTION [Tran1]
+
+BEGIN TRY
+
+--INSERT INTO [Test].[dbo].[T1]
+--  ([Title], [AVG])
+--VALUES ('Tidd130', 130), ('Tidd230', 230)
+
+--UPDATE [Test].[dbo].[T1]
+--  SET [Title] = N'az2' ,[AVG] = 1
+--WHERE [dbo].[T1].[Title] = N'az'
+
+
+COMMIT TRANSACTION [Tran1]
+
+END TRY
+BEGIN CATCH
+  ROLLBACK TRANSACTION [Tran1]
+END CATCH  
